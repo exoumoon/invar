2e54673c9763879756ad3b2e85f13ef96d437cc0
mxxntype <59417007+mxxntype@users.noreply.github.com>
2025-10-06 01:17:20 +0300

    chore(deps): Bump Rust toolchain

--- packages/default/default.nix
+++ packages/default/default.nix
@@ -3,7 +3,7 @@
 let
     toolchain = inputs.fenix.packages.${pkgs.system}.fromToolchainFile {
         file = ../../rust-toolchain.toml;
-        sha256 = "sha256-xpStU6xQanJNSXnOU9AY7nz9Ycjlv0/eQkNHP1LSBoc=";
+        sha256 = "sha256-SIPrdZidk0qhpUlQU6Ya/Cy/E5Dv8GHROdoh5XaFm6I=";
     };
 
     naersk' = pkgs.callPackage inputs.naersk {
